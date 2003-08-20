38f63a6c4647b2123e13a41c2c018005767cce0d
kedder <kedder>
2003-08-20 19:32:07 +0000

    trove added

--- setup.py
+++ setup.py
@@ -14,23 +14,44 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: setup.py,v 1.1 2003/08/05 18:32:16 kedder Exp $
+# $Id: setup.py,v 1.2 2003/08/20 19:32:07 kedder Exp $
 
 from distutils.core import setup
 from kedpm import __version__
 import os, sys
 
 def main():
+    
+    long_description = """Ked Password Manager helps to manage large amounts of
+passwords and related information and simplifies tasks of searching and
+entering password data.
+
+Ked-PM written as extensible framework, which allows to plug in custom password
+database back-ends and custom user interface front-ends. Currently only Figaro
+PM back-end and command line interface front-end supported."""
+    
     setup(
         name="kedpm",
         version=__version__,
         description="Ked Password Manager",
+        long_description = long_description,
         author="Andrey Lebedev",
-        author_email="andrey@micro.lt",
+        author_email="andrey@users.sourceforge.net",
         url="http://kedpm.sourceforge.net/",
         packages=['kedpm', 'kedpm.plugins', 'kedpm.frontends'],
         scripts=['kedpm-cli'],
-        data_files=[(os.path.join('share', 'kedpm'), ['AUTHORS', 'COPYING', 'INSTALL'])]
+        data_files=[(os.path.join('share', 'kedpm'), ['AUTHORS', 'COPYING', 'INSTALL'])],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Topic :: Security',
+            'Topic :: Utilities'
+        ]
     )
 
 if __name__ == '__main__':
